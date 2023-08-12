b3598d16f9954cf884a395293216345b4e96cecd
Ahmed <ahmed>
2023-08-12 01:23:16 -0400

    update router3

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,23 @@
 <script>
-	import { Router, Route } from 'svelte-routing';
-	import Header from './components/Header.svelte'
-	// @ts-ignore
-	import Home from './pages/Home.svelte';
-	import Projects from './pages/Projects.svelte';
-	// import Resume from './pages/Resume.svelte';
-	// import Contact from './pages/Contact.svelte';
-	// import Star from './MyProjects/All-Star/NBA.svelte';
-	// import Klay from './MyProjects/Klay/klay.svelte';
-  </script>
-  <header>
-	<Header/>
-  </header>
-<body>
-	<Router>
-		<Route path="/" component={Home} />
-		
-		<Route path="/projects" component={Projects} />
-		<!-- <Route path="/resume" component={Resume} /> -->
-		<!-- <Route path="/contact" component={Contact} /> -->
-		<!-- <Route path="/allStar" component={Star} /> -->
-		<!-- <Route path="/klay" component={Klay} /> -->
-	  </Router>
-	  
-	
-</body>
-<!-- <script>
-	import { Router, Link, Route } from 'svelte-navigator';
+	import Router, {location, link} from 'svelte-spa-router';
 	import Header from './components/Header.svelte';
 	import Home from './pages/Home.svelte';
 	import Projects from './pages/Projects.svelte';
-	// import Resume from './pages/Resume.svelte';
-	// import Contact from './pages/Contact.svelte';
-	// import Star from './MyProjects/All-Star/NBA.svelte';
-	// import Klay from './MyProjects/Klay/klay.svelte';
   </script>
   
   <header>
 	<Header />
   </header>
   
-
   <body>
-
-	<Router>
-			<Route path="/" component={Home} />
-			<Route path="/projects" component={Projects} />
-	</Router>
-  </body> -->
-
+	<nav>
+		<a href="/">Home</a>
+		<a href="/#/projects">Projects</a>
+	</nav>
+	
+	<Router routes={{
+		'/': Home,
+		'/projects': Projects
+	}} />
+  </body>
+  
